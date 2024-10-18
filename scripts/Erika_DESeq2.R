cb0be692459403b2e11e320a1a90a35e01f84eef
Alice Balard <alice.cam.balard@gmail.com>
2024-10-18 16:06:41 +0100

    up

--- scripts/Erika_DESeq2.R
+++ scripts/Erika_DESeq2.R
@@ -12,7 +12,7 @@ setwd("/Users/akire/Documents/IGB-tasks/Transcriptome_assay/diff_exp/data")
 # Data loading
 ## case 1- one alignment 
 ### Load the count matrix
-datar <- read.table("RSEM.gene.counts.matrix", header=T)
+count_matrixr <- read.table("RSEM.gene.counts.matrix", header=T)
 
 ### Load all the samples
 samples_data <- read.table("/Users/akire/Documents/IGB-tasks/Transcriptome_assay/diff_exp/required_tables/sample_data_r.txt", header = TRUE, sep = "\t")
@@ -30,13 +30,11 @@ count_matrixr_cyanogenome <- count_matrixr_cyanogenome[grep("cyano|both", names(
 # RSEM_cyano _chytri _both_cyano _both_chytrid
 datar_chytrid <- read.table("RSEM_chytrid.gene.counts.matrix", header=T)
 datar_both_chytrid <- read.table("RSEM_both_chytrid.gene.counts.matrix", header=T)
-## TO CHECK
-count_matrixr_chytridgenome_CASE2 <-rbind(datar_chytrid, datar_both_chytrid)
+count_matrixr_chytridgenome_CASE2 <-cbind(datar_chytrid, datar_both_chytrid)
 
 datar_cyano <- read.table("RSEM_cyano.gene.counts.matrix", header=T)
 datar_both_cyano <- read.table("RSEM_both_cyano.gene.counts.matrix", header=T)
-## TO CHECK
-count_matrixr_cyanogenome_CASE2 <- rbind(datar_cyano, datar_both_cyano)
+count_matrixr_cyanogenome_CASE2 <- cbind(datar_cyano, datar_both_cyano)
 
 #################
 # DESeq2 analysis- for this analysis I need un-normalized counts. 
@@ -57,18 +55,22 @@ calculateContrasts <- function(my_samples, my_countsmatrix, my_org){
   vstr$Gene <- rownames(vstr)
   
   ## Calculate the contrasts (pairwise comparisons of interest)
+  print(paste0("comparison of groups: ", my_groups[1], " vs ", my_groups[2]))
   resr_met_effect_2orgs <- results(ddsr, contrast=c("condition", my_groups[1], my_groups[2]), alpha=0.05) 
   print(summary(resr_met_effect_2orgs))
   print(sum(resr_met_effect_2orgs$padj < 0.05, na.rm=TRUE))
   
+  print(paste0("comparison of groups: ", my_groups[3]," vs ", my_groups[4]))
   resr_met_effect_1org <- results(ddsr, contrast=c("condition", my_groups[3], my_groups[4]), alpha=0.05) 
   print(summary(resr_met_effect_1org))
   print(sum(resr_met_effect_1org$padj < 0.05, na.rm=TRUE))
   
+  print(paste0("comparison of groups: ", my_groups[3], " vs ", my_groups[1]))
   resr_inf_effect_control <- results(ddsr, contrast=c("condition", my_groups[3], my_groups[1]), alpha=0.05) 
   print(summary(resr_inf_effect_control))
   print(sum(resr_inf_effect_control$padj < 0.05, na.rm=TRUE))
   
+  print(paste0("comparison of groups: ", my_groups[4], " vs ", my_groups[2]))
   resr_inf_effect_met <- results(ddsr, contrast=c("condition", my_groups[4], my_groups[2]), alpha=0.05) 
   print(summary(resr_inf_effect_met))
   print(sum(resr_inf_effect_met$padj < 0.05, na.rm=TRUE))
@@ -83,7 +85,7 @@ calculateContrasts <- function(my_samples, my_countsmatrix, my_org){
               resr_met_effect_1org=resr_met_effect_1org,
               resr_inf_effect_control=resr_inf_effect_control,
               resr_inf_effect_met=resr_inf_effect_met,
-              vstr=vstr)
+              vstr=vstr))
 }
 
 contrast_cyanogenome_Case1 <- calculateContrasts(
@@ -94,7 +96,7 @@ contrast_cyanogenome_Case1 <- calculateContrasts(
 contrast_chytridgenome_Case1 <- calculateContrasts(
   my_samples=samples_chytrid_genome,
   my_countsmatrix=count_matrixr_chytridgenome,
-  my_org="chytrid")
+  my_org="chy")
 
 contrast_cyanogenome_Case2 <- calculateContrasts(
   my_samples=samples_cyano_genome,
@@ -104,7 +106,7 @@ contrast_cyanogenome_Case2 <- calculateContrasts(
 contrast_chytridgenome_Case2 <- calculateContrasts(
   my_samples=samples_chytrid_genome,
   my_countsmatrix=count_matrixr_chytridgenome_CASE2,
-  my_org="chytrid")
+  my_org="chy")
 
 plotHeatmap <- function(vstr){
   # Overwrite our original data frame with the long format
@@ -121,3 +123,26 @@ plotHeatmap <- function(vstr){
 }
 
 plotHeatmap(contrast_cyanogenome_Case1$vstr)
+plotHeatmap(contrast_cyanogenome_Case2$vstr)
+plotHeatmap(contrast_chytridgenome_Case1$vstr)
+plotHeatmap(contrast_chytridgenome_Case2$vstr)
+
+# heatmap of the sample-to-sample distances
+
+makeCorrelationPlot <- function(my_vstr){
+  my_vstr=my_vstr[!names(my_vstr)%in%"Gene"]
+  
+  sampleDists <- dist(t(my_vstr))
+  
+  sampleDistMatrix <- as.matrix(sampleDists)
+  colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+  pheatmap(sampleDistMatrix,
+           clustering_distance_rows=sampleDists,
+           clustering_distance_cols=sampleDists,
+           col=colors)
+}
+
+makeCorrelationPlot(contrast_cyanogenome_Case1$vstr)
+#makeCorrelationPlot(contrast_cyanogenome_Case2$vstr)
+makeCorrelationPlot(contrast_chytridgenome_Case1$vstr)
+#makeCorrelationPlot(contrast_chytridgenome_Case2$vstr)
