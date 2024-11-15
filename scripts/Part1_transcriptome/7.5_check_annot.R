de1830f2e8f95c5689a5d5c1774b993644eaa279
Alice Balard <alice.cam.balard@gmail.com>
2024-11-15 11:56:34 +0100

    chose eukaryote second filter

--- scripts/Part1_transcriptome/7.5_check_annot.R
+++ scripts/Part1_transcriptome/7.5_check_annot.R
@@ -1,13 +1,7 @@
+## Compare old and new annotations
 oldAnnot <- read.csv("data/allFungiTrinot_simplified.tsv", sep = "\t")
 
 newAnnot <- read.csv("data/assemblyMergedFungi_simplified.tsv", sep = "\t")
-
-head(oldAnnot,1)
-
-
-
-
-test <- newAnnot$sprot_Top_BLASTX_hit[1:10]
 
 ## Remove everything before "Full"; Extract the first word after the first "^"; rm ;
 extractBlastxTaxa <- function(x){
@@ -28,13 +22,56 @@ oldAnnot <- extractBlastxTaxa(oldAnnot)
 newAnnot <- extractBlastxTaxa(newAnnot)
 
 table(oldAnnot$blastxKingdom)
+##     .   Archaea  Bacteria Eukaryota   Viruses 
+##   20863       142      3359     57740       119 
+
 table(newAnnot$blastxKingdom)
+##      .   Archaea  Bacteria Eukaryota   Viruses 
+##  48688       192      5801     61449       165 
+
+## NB duplicated isoforms
 
 table(oldAnnot$blastxKingdom, oldAnnot$blastxPhylum)
 table(newAnnot$blastxKingdom, newAnnot$blastxPhylum)
 
-table(newAnnot$blastxPhylum[
-               newAnnot$blastxKingdom %in% "Eukaryota"])
+nrow(newAnnot[newAnnot$blastxKingdom %in% "Eukaryota",])/
+    nrow(newAnnot) *100
+## 52.8% is annotated with Eukaryota
 
-table(newAnnot$blastxPhylum[
-               newAnnot$blastxKingdom %in% "Bacteria"])
+eukTransc <- newAnnot[newAnnot$blastxKingdom %in% "Eukaryota","transcript_id"]
+
+nrow(newAnnot[newAnnot$blastxPhylum %in% "Fungi",])/
+    nrow(newAnnot) *100
+## 25% is annotated with Fungi
+
+eukTransc <- newAnnot[newAnnot$blastxKingdom %in% "Eukaryota","transcript_id"]
+
+length(eukTransc);length(unique(eukTransc))
+
+fungTransc <- newAnnot[newAnnot$blastxPhylum %in% "Fungi","transcript_id"]
+
+length(fungTransc);length(unique(fungTransc))
+
+#################################################
+## Subset our transcriptome for these transcripts
+library("seqinr")
+
+transc <- read.fasta("/scratch/alicebalard/outData/assemblyMergedFungi/trinity_out_dir/Trinity.fasta")
+
+transc1 <- transc[names(transc) %in% eukTransc]
+
+write.fasta(transc1,
+            names(trans1),
+            file.out= "/scratch/alicebalard/outData/assemblyMergedFungi/trinity_out_dir/Trinity_eukaryoteHits.fasta")
+
+write.fasta(transc[names(transc) %in% fungTransc],
+            names(transc[names(transc) %in% fungTransc]),
+            file.out= "/scratch/alicebalard/outData/assemblyMergedFungi/trinity_out_dir/Trinity_fungiHits.fasta")
+
+###############################################
+## Subset gene-trans-map for the Eukaryotic one
+
+gtm <- read.table("/scratch/alicebalard/outData/assemblyMergedFungi/trinity_out_dir/Trinity.fasta.gene_trans_map") 
+
+write.table(gtm[gtm$V2 %in% eukTransc,], "/scratch/alicebalard/outData/assemblyMergedFungi/trinity_out_dir/Trinity_eukaryoteHits.fasta.gene_trans_map", row.names=F, quote=F, col.names = F)
+
