52688b7fcc5e98bf0aae653f2277e24dcb8796f5
Alice Balard <alice.cam.balard@gmail.com>
2026-04-14 13:55:08 +0200

    update diamond blastX against nr and not uniprot

--- scripts/Part1_transcriptome/5.1_diamondBlastX.sh
+++ scripts/Part1_transcriptome/5.1_diamondBlastX.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=alicebalard@zedat.fu-berlin.de  
 #SBATCH --mail-type=end
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=24
 #SBATCH --mem=500GB 
 #SBATCH --time=2-24:00:00
 #SBATCH --qos=standard              
@@ -28,20 +28,32 @@ cd /scratch/alicebalard/outData/diamondBlastX
 num_threads=$SLURM_CPUS_PER_TASK
 
 module load BLAST+/2.13.0-gompi-2022a
-module load DIAMOND/2.0.13-GCC-11.2.0
+module load DIAMOND/2.1.8-GCC-12.3.0
 
-## NCBI BLAST searches must be run with an appropriate output format in order to be imported dl database in nt repo: wget "ftp://ftp.ncbi.nlm.nih.gov/blast/db/nt.*.tar.gz" -P nt/ && for file in nt/*.tar.gz; do tar xf $file -C nt && rm $file; done
+echo "Download databases:"
+wget -P /scratch/alicebalard/resources -c http://ftp.ncbi.nlm.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.FULL.gz ## can be used zipped
 
-## echo "Make diamond db..."
-## diamond makedb -p $num_threads --in /scratch/alicebalard/resources/uniprot/reference_proteomes.fasta.gz --taxonmap /scratch/alicebalard/resources/uniprot/reference_proteomes.taxid_map --taxonnodes /scratch/alicebalard/resources/taxdump/nodes.dmp --taxonnames /scratch/alicebalard/resources/taxdump/names.dmp --db /scratch/alicebalard/resources/uniprot/reference_proteomes.dmnd
-## echo "diamond db done"
+wget -P /scratch/alicebalard/resources -c http://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdmp.zip
+gzip -d /scratch/alicebalard/resources/taxdmp.zip ## needs unzipped to access sub files
 
+wget -P /scratch/alicebalard/resources -c https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz ## can be used zipped
+
+echo "Make diamond db..."
+diamond makedb -p $num_threads\
+  --in /scratch/alicebalard/resources/nr.gz \
+  --db /scratch/alicebalard/resources/nr \
+  --taxonmap /scratch/alicebalard/resources/prot.accession2taxid.FULL.gz \
+  --taxonnodes /scratch/alicebalard/resources/taxdmp/nodes.dmp \
+  --taxonnames /scratch/alicebalard/resources/taxdmp/names.dmp
+echo "diamond db done"
+
+echo "Start diamond blastX search..."
 OUT=/scratch/alicebalard/outData/diamondBlastX
-DB=/scratch/alicebalard/resources/uniprot/reference_proteomes.dmnd
+DB=/scratch/alicebalard/resources/nr.dmnd
 
 ###########################################
 echo "First assembly: Z1 to Z12, only chytrids"
-ASSEMBLY=/scratch/alicebalard/outData/assembly/trinity_out_dir/Trinity.fasta
+ASSEMBLY=/scratch/alicebalard/outData/assemblies/assembly_Z/Trinity.fasta
 
 echo "Create DIAMOND hits..."
 ## Run diamond
@@ -52,14 +64,12 @@ diamond blastx --query $ASSEMBLY \
 	--max-target-seqs 1 \
         --evalue 1e-5 \
         --threads $num_threads \
-        > $OUT/assemblyZ_diamond_1e-5pval.out
-echo "done."
-
+        > $OUT/assemblyZ_diamondNR_1e-5pval.out
 echo "done."
 	
 ##############################################################
-echo "Second assembly: In1 to In12, chytrids infected by bacteria"
-ASSEMBLY=/scratch/alicebalard/outData/assembly_In/trinity_out_dir/Trinity.fasta
+echo "Second assembly: In1 to In12, chytrids infected by bacteria, & coculture"
+ASSEMBLY=/scratch/alicebalard/outData/assemblies/assembly_In_coculture/Trinity.fasta
 
 echo "Create DIAMOND hits..."
 ## Run diamond
@@ -70,5 +80,5 @@ diamond blastx --query $ASSEMBLY \
 	--max-target-seqs 1 \
         --evalue 1e-5 \
         --threads $num_threads \
-        > $OUT/assemblyIn_diamond_1e-5pval.out
+        > $OUT/assemblyIn_cocult_diamondNR_1e-5pval.out
 echo "done."
